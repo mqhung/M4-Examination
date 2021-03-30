10f9b31e64fe8ec8ec8c15c1ca7f01a3eddb5c99
Hung <mqhung93@gmail.com>
2021-03-30 21:46:35 +0700

    create model city and nation

---
+++ src/main/java/com/module4/exam/model/City.java
@@ -0,0 +1,88 @@
+package com.module4.exam.model;
+
+import javax.persistence.*;
+
+@Entity
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Long area;
+    private Long population;
+    private Long gdp;
+    private String description;
+
+    @ManyToOne
+    private Nation nation;
+
+    public City(Long id, String name, Long area, Long population, Long gdp, String description, Nation nation) {
+        this.id = id;
+        this.name = name;
+        this.area = area;
+        this.population = population;
+        this.gdp = gdp;
+        this.description = description;
+        this.nation = nation;
+    }
+
+    public City() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getArea() {
+        return area;
+    }
+
+    public void setArea(Long area) {
+        this.area = area;
+    }
+
+    public Long getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(Long population) {
+        this.population = population;
+    }
+
+    public Long getGdp() {
+        return gdp;
+    }
+
+    public void setGdp(Long gdp) {
+        this.gdp = gdp;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Nation getNation() {
+        return nation;
+    }
+
+    public void setNation(Nation nation) {
+        this.nation = nation;
+    }
+}
